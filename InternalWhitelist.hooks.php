29827b6d8df00af70e8a49b03d6c0d4be9bf2c09
Stanislav Pika <staspika@gmail.com>
2017-04-04 17:00:56 +0200

    Modified the parser to bring it into accord with list syntax in MediaWiki (https://www.mediawiki.org/wiki/Help:Lists): a list item cannot start with a whitespace.

--- InternalWhitelist.hooks.php
+++ InternalWhitelist.hooks.php
@@ -9,7 +9,7 @@ class InternalWhitelistHooks {
         $pagearray = explode("\n", wfMessage( 'Whitelist' )->text());
         foreach($pagearray as $arg ) {
             # Find lines starting with one or more `*`, preceeded by zero or more whitespaces
-            $has_match = preg_match('/\s*\**s(.*)/', $arg, $matches);
+            $has_match = preg_match('/\**s\s*(.*)/', $arg, $matches);
             if ($has_match == 1) {
                 $wgWhitelistRead[]=trim(trim($arg, "*"));
             }
