99cc0853804630f71370990aa334b44689c7ac1f
Stanislav Pika <staspika@gmail.com>
2017-04-04 11:42:54 +0200

    New syntax for the whitelist: only lines starting with one or more `*` preceeded by zero or more whitespaces are treated as whitelist entries, all other lines are ignored.

--- InternalWhitelist.hooks.php
+++ InternalWhitelist.hooks.php
@@ -2,18 +2,18 @@
 class InternalWhitelistHooks {
     public static function onUserGetRights($user, &$aRights){
         global $wgWhitelistRead;
-        /** if user is not anonymous, then exit the script **/
-        if(!$user->isAnon()){
+        # If user is not anonymous, then exit the script
+        if( !$user->isAnon() ) {
             return true;
         }
-        $pagearray = explode("\n", wfMessage( 'Whitelist' )->inContentLanguage()->text() );
-        foreach($pagearray as $arg){
-            if (strpos($arg, '//') !== false) {
-                continue;
+        $pagearray = explode("\n", wfMessage( 'Whitelist' )->text());
+        foreach($pagearray as $arg ) {
+            # Find lines starting with one or more `*`, preceeded by zero or more whitespaces
+            $has_match = preg_match('/\s*\**s(.*)/', $arg, $matches);
+            if ($has_match == 1) {
+                $wgWhitelistRead[]=trim(trim($arg, "*"));
             }
-            $wgWhitelistRead[]=trim(trim($arg, "*"));
         }
         return true;
     }
 }
-
